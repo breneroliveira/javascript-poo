aa03c851df660e494ab217b87683e956abb2ad01
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2022-03-31 10:29:55 -0300

    Tipos de dados

---
+++ Exemplos/1_Tipos de dados/scripts.js
@@ -0,0 +1,45 @@
+// TIPOS PRIMITIVOS
+var valor = 18;
+var sexo = 'f';
+var nome = 'Rodrigo';
+var existe = false;
+var endereco = null;
+
+var primeiroChar = nome.charAt(2);
+var varNomeMaiusculo = nome.toLocaleUpperCase();
+
+//console.log(!!existe);
+//console.log(valor.toFixed(2));
+
+/**/
+
+// TIPOS DE REFERÊNCIA (CONSTRUÇÃO DE OBJETOS)
+var pessoa = new Object(); // Usou o método construtor para criar o objeto.
+
+var pessoa2 = {
+    'nome':'Rodrigo',
+    'idade':'21',
+    'sexo':'M'
+}; // Usou a forma literal para constuir o objeto.
+
+console.log(pessoa2);
+
+//--------------------------------------------------------------------
+
+var mostraNome = new Function("console.log('Rodrigo');"); // Usou o método construtor para criar a função.
+mostraNome(); // Chama a função.
+
+function mostraIdade() {
+    console.log('Idade é igual a ' + 15 + '.');
+} // Usou a forma literal para constuir a função.
+mostraIdade();
+
+//--------------------------------------------------------------------
+
+var carro = new Array('Gol', 'Polo', 'Focus'); // Usou o método construtor para criar o vetor.
+console.log(carro); // Mostra o vetor.
+
+var carros = ['Corsa', 'Celta', 'Palio']; // Usou a forma literal para constuir o vetor.
+console.log(carros);
+
+//--------------------------------------------------------------------
