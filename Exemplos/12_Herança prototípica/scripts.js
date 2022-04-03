5fcca876e739cdac3d0a16f899873b9f745740fb
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2022-04-03 20:36:04 -0300

    Herança prototípica

---
+++ Exemplos/12_Herança prototípica/scripts.js
@@ -0,0 +1,37 @@
+function Pessoa(nome, idade, sexo) { // Construtor.
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+}
+
+var makePessoa = function(nome, idade, sexo) { // Factory function.
+    return { // Retorna o objeto.
+        constructor:makePessoa,
+
+        nome:nome,
+        idade:idade,
+        sexo:sexo,
+        getIdade:function() {
+            return this.idade;
+        }
+    }
+}
+
+var rodrigo = new Pessoa('Rodrigo', 21, 'M'); // Instância de Pessoa, objeto.
+var roberto = makePessoa('Roberto', 32, 'M');
+
+var pessoaCriada = Object.create(roberto, { // Herda as propriedades de makePessoa, a única propriedade exclusiva de pessoaCriada é endereco.
+    endereco: {
+        value:'Rua Tal',
+        writable:true,
+        configurable:true,
+        enumerable:true
+    }
+});
+
+console.log(roberto.getIdade());
+console.log(pessoaCriada);
+console.log(pessoaCriada.getIdade());
+
+//console.log(rodrigo, roberto);
+//console.log(Object.getPrototypeOf(roberto));
