f63892d708f8de08aee244231dc686492c9876bf
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2022-04-04 11:01:54 -0300

    Update scripts.js

--- Exemplos/5_Entendendo objetos/scripts.js
+++ Exemplos/5_Entendendo objetos/scripts.js
@@ -10,15 +10,17 @@ if(/*"casado" in pessoa ou*/ pessoa.hasOwnProperty("casado")) { // Verifica se h
     delete pessoa.casado;
 }
     
-/*for(propriedade in pessoa) {
+for(propriedade in pessoa) {
     console.log(propriedade);
     console.log(pessoa[propriedade]);
-}*/
+}
+
+console.log('-----------------------------------------------------------');
 
 //console.log(pessoa.hasOwnProperty("casado")); // Serve para saber se a propriedade existe em um objeto.
 //console.log(pessoa);
 
-/*var i, size;
+var i, size;
 var propridade = Object.keys(pessoa);
 
 for(i = 0, size = propridade.length; i < size; i++) {
@@ -26,7 +28,9 @@ for(i = 0, size = propridade.length; i < size; i++) {
     console.log(pessoa[propridade[i]]); // Mostra o valor da propriedade.
 }
 
-console.log(propridade);*/
+console.log('-----------------------------------------------------------');
+
+console.log(propridade);
 
 console.log(pessoa.propertyIsEnumerable("lengh")); // Propriedade não enumerável.
 
