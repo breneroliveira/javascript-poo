50566e6552b4f015634b33630e7030da27094e0d
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2022-04-08 00:54:02 -0300

    Update ex_mamifero.js

--- Exemplos/Heranca/ex_mamiferos/ex_mamifero.js
+++ Exemplos/Heranca/ex_mamiferos/ex_mamifero.js
@@ -1,32 +1,36 @@
+// Classe pai.
 class Mamifero {
     constructor(especie, nome, idade) {
-        this.especie = especie
-        this.nome = nome
-        this.idade = idade
-        this.glandulaMamaria = true
+        this.especie = especie;
+        this.nome = nome;
+        this.idade = idade;
+        this.glandulaMamaria = true;
     }
 
-    incrementarIdade() {
-        this.idade++
+    incrementarIdade() { // Método.
+        this.idade++;
     }
 }
 
+// Classe filha (herdada).
 class Leao extends Mamifero {
     constructor(especie, nome, idade, comerHumano) {
-        super(especie, nome, idade)
-        this.comerHumano = comerHumano
+        super(especie, nome, idade); // Serve para não precisar colocar as propriedades da classe pai novamente.
+        // Invoca o constructor da classe pai, da classe Mamifero.
+        this.comerHumano = comerHumano;
     }
 
     comerZebras(animais) {
-        return animais.filter(animal => animal.especie !== 'zebra')
+        return animais.filter(animal => animal.especie !== 'zebra');
     }
 }
 
-const zeca = new Mamifero('zebra', 'Zeca', 6)
-const pompeu = new Mamifero('gnu', 'Pompeu', 5)
-const angus =  new Mamifero('cavalo', 'Angus', 3)
-const mufasa = new Mamifero('leão', 'Mufasa', 7, false)
+// Objeto a partir da classe Mamifero.
+const zeca = new Mamifero('zebra', 'Zeca', 6);
+const pompeu = new Mamifero('gnu', 'Pompeu', 5);
+const angus =  new Mamifero('cavalo', 'Angus', 3);
+const mufasa = new Mamifero('leão', 'Mufasa', 7, false);
 
-const animais = [zeca, pompeu, angus]
+const animais = [zeca, pompeu, angus];
 
-console.log(mufasa)
+console.log(mufasa);
